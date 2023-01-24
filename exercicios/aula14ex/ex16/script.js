50c5aa2776ebb3db7dd29a3c8f767704ff0d6ef1
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-01-24 05:47:36 -0300

    deu tudo errado

--- exercicios/aula14ex/ex16/script.js
+++ exercicios/aula14ex/ex16/script.js
@@ -2,4 +2,13 @@ function contar() {
     var inicio = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    var inicio = Number(inicio.value)
+    var fim = Number(fim.value)
+    var passo = Number(passo.value)
+
+    for (inicio; inicio != fim; passo+=passo) {
+        res.innerHTML = `${passo}`
+    }
 }
