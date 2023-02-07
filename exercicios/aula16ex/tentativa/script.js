9fa3ceeeeef0aa7f107e2654c10ba9df2d08d6c1
arnaldohenrique <109180588+arnaldohenrique@users.noreply.github.com>
2023-02-07 07:17:38 -0300

    consegui na quelas

--- exercicios/aula16ex/tentativa/script.js
+++ exercicios/aula16ex/tentativa/script.js
@@ -1,23 +1,54 @@
+let num = document.getElementById('num')
+let ana = document.getElementById('analisa')
+let res = document.getElementById('res')
+let valores = []
+
 function adiciona() {
-    var num = document.getElementById('num')
-    var ana = document.getElementById('analisa')
-
     if (num.value.length == 0) return window.alert('Por favor, digite um número!'); //caso não aja valores
-    var n = Number(num.value)
+    let n = Number(num.value)
 
-    if (n > 100 || n < 1) return window.alert('Digite um valor aceitável!');
-        
-    var item = document.createElement('option')
-    item.text = `Valor ${n} adicionado`
-    ana.appendChild(item)
+    if (n <= 100 && n >= 1 && valores.indexOf(n) == -1) {
+
+        valores.push(n)
+
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado`
+        ana.appendChild(item)
+        res.innerHTML = ''
+    }else {
+        window.alert('Digite um valor aceitável!')
+    }
+
+    num.value =''
+    num.focus()
 }
 
-function finaliza() {
-    var f = [n]
-    var res = document.getElementById('res')
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma+=valores[pos]
+            if(valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
 
+        media = soma / tot
 
-
-    res.innerHTML = `Ao todo temos ${f.length} números cadastrados aqui`
-
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}.</p>`
+        res.innerHTML += `<p>O maior número é ${maior}.</p>`
+        res.innerHTML += `<p>O menor número é ${menor}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+    }
 }
